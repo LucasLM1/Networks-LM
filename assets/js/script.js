a6c2aa456fd5e4f48915526f073251f65f6a4d54
Lucas Souza <lucascorreia325@gmail.com>
2024-06-24 18:33:04 -0300

    feat: new version

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,12 +1,36 @@
+//Tema dark e light
+const themeButton = document.getElementById('theme-button')
+const lightTheme = 'dark-theme'
+const iconTheme = 'bx-sun'
+
+const selectedTheme = localStorage.getItem('selected-theme')
+const selectedIcon = localStorage.getItem('selected-icon')
+
+const getCurrentTheme = () => document.body.classList.contains(lightTheme) ? 'dark' : 'light'
+const getCurrentIcon = () => themeButton.classList.contains(iconTheme) ? 'bx bx-sun' : 'bx bx-sun'
+
+if (selectedTheme) {
+    document.body.classList[selectedTheme === 'dark' ? 'add' : 'remove'](lightTheme)
+    themeButton.classList[selectedIcon === 'bx bx-sun' ? 'add' : 'remove'](iconTheme)
+}
+
+themeButton.addEventListener('click', () => {
+    document.body.classList.toggle(lightTheme)
+    themeButton.classList.toggle(iconTheme)
+    localStorage.setItem('selected-theme', getCurrentTheme())
+    localStorage.setItem('selected-icon', getCurrentIcon())
+});
+
 window.sr = ScrollReveal({ reset: true });
 
 const elements = [
     document.querySelector('header'),
+    document.querySelector('footer'),
+    
     document.querySelector('.social-network__linkedin'),
     document.querySelector('.social-network__github'),
     document.querySelector('.social-network__instagram'),
-    document.querySelector('.curriculum'),
-    document.querySelector('footer'),
+    document.querySelector('.social-network_portfolio')
 ]
 
 var cascading = {
@@ -15,9 +39,9 @@ var cascading = {
     distance: '25px'
 }
 
-sr.reveal(elements, cascading)
+sr.reveal(elements, cascading);
 
-sr.reveal('.social-network__linkedin', {
+sr.reveal('.social-network__linkedin',{
     delay: 1000,
 })
 
@@ -29,39 +53,39 @@ sr.reveal('.social-network__instagram',{
     delay: 2000
 })
 
-sr.reveal('.curriculum',{
+sr.reveal('.social-network__portfolio',{
     delay: 2500
 })
 
 sr.reveal('footer',{
     delay: 3000
-})
+});
 
-const typing = document.querySelector('[data-js="typing"]');
+// const typing = document.querySelector('[data-js="typing"]');
 
-const messages = [
-    'Desenvolvido por: Lucas M. Souza',
-    'Entre em contato comigo através de uma das minhas redes sociais', 
-    'Obrigado por visitar ❤️'
-];
+// const messages = [
+//     'Desenvolvido por: Lucas M. Souza',
+//     'Entre em contato comigo através de uma das minhas redes sociais', 
+//     'Obrigado por visitar ❤️'
+// ];
 
-let messageIndex = 0;
-let characterIndex = 0;
-let currentMessage = '';
-let currentCharacters = '';
+// let messageIndex = 0;
+// let characterIndex = 0;
+// let currentMessage = '';
+// let currentCharacters = '';
 
-const type = () => {
-    if(messageIndex === messages.length){
-        messageIndex = 0
-    }
+// const type = () => {
+//     if(messageIndex === messages.length){
+//         messageIndex = 0
+//     }
 
-    currentMessage = messages[messageIndex]
-    currentCharacters = currentMessage.slice(0, characterIndex++)
-    typing.textContent = currentCharacters
+//     currentMessage = messages[messageIndex]
+//     currentCharacters = currentMessage.slice(0, characterIndex++)
+//     typing.textContent = currentCharacters
 
-    if(currentCharacters.length === currentMessage.length){
-        messageIndex++
-        characterIndex = 0
-    }
-}
-setInterval(type, 200)
+//     if(currentCharacters.length === currentMessage.length){
+//         messageIndex++
+//         characterIndex = 0
+//     }
+// }
+// setInterval(type, 200)
